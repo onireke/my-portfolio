61279f43a70b30d8fc504f3b157601598d03472f
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-11-23 15:09:13 +0100

    fix: fixing

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -5,30 +5,35 @@ import ProjectCard from "./ProjectCard";
 function ProjectList() {
   const projects = [
     {
+      id: 1,
       title: "Global finbus",
       img: "/images/globalfinbus.jpeg",
       desc: "We help you build the management system",
       tech: ["nextjs", "Tailwind", "Typscript", "scss"],
     },
     {
+      id: 2,
       title: "Cocktail",
       img: "/images/coctailimg.webp",
       desc: "ehtgdiluqwehf;oquowhqywefhwqohfouwbefboe behtgdiluqwehf;oquowhqywefhwqohfouwbefboeb",
       tech: ["reactjs", "scss"],
     },
     {
+      id: 3,
       title: "Global finbus",
       img: "/images/globalfinbus.jpeg",
       desc: "We help you build the management system",
       tech: ["nextjs", "Tailwind", "Typscript", "scss"],
     },
     {
+      id: 4,
       title: "Global finbus",
       img: "/images/globalfinbus.jpeg",
       desc: "We help you build the management system",
       tech: ["nextjs", "Tailwind", "Typscript", "scss"],
     },
     {
+      id: 5,
       title: "Global finbus",
       img: "/images/globalfinbus.jpeg",
       desc: "We help you build the management system",
@@ -37,9 +42,9 @@ function ProjectList() {
   ];
   return (
     <main className="project--list">
-      {projects.map((p) => (
+      {projects.map((p, i) => (
         <ProjectCard
-          key={p.title}
+          key={i}
           title={p.title}
           img={p.img}
           desc={p.desc}
