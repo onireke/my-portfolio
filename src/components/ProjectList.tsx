7b3a147582094e447660ef6a804b230b7ca20385
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-12-14 15:49:53 +0100

    update

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import ProjectCard from "./ProjectCard";
+import Link from "next/link";
 
 function ProjectList() {
   const projects = [
     {
       id: 1,
       title: "Global finbus",
-      img: "/images/globalfinbus.jpeg",
+      img: "/images/globalfinbus.png",
       desc: "We help you build the management system",
       tech: ["nextjs", "Tailwind", "Typscript", "scss"],
+      link: "",
     },
     {
       id: 2,
@@ -17,6 +19,7 @@ function ProjectList() {
       img: "/images/cocktail.png",
       desc: "our cocktail is usually an iced drink of wine mixed with flavoring ingredients",
       tech: ["reactjs", "scss"],
+      link: "https://my-sweet-cock-tail-app.netlify.app/",
     },
     {
       id: 3,
@@ -24,18 +27,22 @@ function ProjectList() {
       img: "/images/productimage.png",
       desc: "our cocktail is usually an iced drink of wine mixed with flavoring ingredients",
       tech: ["reactjs", "scss", "typescript"],
+      link: "",
     },
   ];
   return (
     <main className="project--list">
       {projects.map((p) => (
-        <ProjectCard
-          key={p.title}
-          title={p.title}
-          img={p.img}
-          desc={p.desc}
-          tech={p.tech}
-        />
+        <Link key={p.title} href={p.link}>
+          <a target="_blank" rel="noreferrer">
+            <ProjectCard
+              title={p.title}
+              img={p.img}
+              desc={p.desc}
+              tech={p.tech}
+            />
+          </a>
+        </Link>
       ))}
     </main>
   );
